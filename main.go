d528feb570e5daa7396de73bbbdf8dda1a9ea96b
xygn99b <jevocity04@gmail.com>
2024-08-21 23:24:16 +0100

    fix: turn ending prematurely when column is full

--- main.go
+++ main.go
@@ -4,27 +4,35 @@ import "fmt"
 
 func main() {
 	board := NewBoard()
-	turn := 0
+	turnNumber := 0
+	turnOver := false
 	token := TokenRed
 	for {
+		if turnOver {
+			turnNumber++
+			if turnNumber%2 == 0 {
+				token = TokenRed
+			} else {
+				token = TokenBlue
+			}
+			turnOver = false
+		}
+
 		print(board.Representation())
-		println("Place your token")
+		fmt.Printf("%c: Place your token", token)
 		var location int
 		if _, err := fmt.Scan(&location); err != nil {
 			panic(err)
 		}
-		turn++
-		turn %= 2
-		if turn%2 == 0 {
-			token = TokenRed
-		} else {
-			token = TokenBlue
-		}
+
 		row, err := board.Place(Token(token), location-1)
 		if err == ErrorColumnFull {
 			println("The selected column is full.")
 			continue
 		}
+
+		turnOver = true
+
 		win := board.CheckWin([2]int{location - 1, row})
 		if win {
 			print(board.Representation())
